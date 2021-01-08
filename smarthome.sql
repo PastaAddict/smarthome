c318d2aad99fe2d3904e342fdde4a16ee04bd35c
PastaAddict <45083577+PastaAddict@users.noreply.github.com>
2021-01-08 04:42:52 +0200

    Update smarthome.sql

--- smarthome.sql
+++ smarthome.sql
@@ -1,7 +1,7 @@
 CREATE TABLE  "Δευτερεύον_προφίλ" (
 	"username_de"	varchar(30) NOT NULL,
 	PRIMARY KEY("username_de"),
-	FOREIGN KEY("username_de") REFERENCES "Προφίλ χρήστη"("username") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("username_de") REFERENCES "Προφίλ_χρήστη"("username") ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE  "Ελέγχει" (
 	"command_id_ελέγχει"	integer NOT NULL,
@@ -19,15 +19,15 @@ CREATE TABLE  "Έχει_πρόσβαση" (
 	"username_πρόσβασης"	varchar(30) NOT NULL,
 	"device_id_πρόσβασης"	varchar(30) NOT NULL,
 	PRIMARY KEY("username_πρόσβασης","device_id_πρόσβασης"),
-	FOREIGN KEY("username_πρόσβασης") REFERENCES "Δευτερεύον προφίλ"("username_de") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("username_πρόσβασης") REFERENCES "Δευτερεύον_προφίλ"("username_de") ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY("device_id_πρόσβασης") REFERENCES "Συσκευή"("device_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE  "Παρέχει_δικαιώματα" (
 	"primary_username"	varchar(30) ,
 	"secondary_username"	varchar(30) NOT NULL,
 	PRIMARY KEY("primary_username","secondary_username"),
-	FOREIGN KEY("primary_username") REFERENCES "Πρωτεύον προφίλ"("username_pro") ON DELETE SET NULL ON UPDATE CASCADE,
-	FOREIGN KEY("secondary_username") REFERENCES "Δευτερεύον προφίλ"("username_de") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("primary_username") REFERENCES "Πρωτεύον_προφίλ"("username_pro") ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY("secondary_username") REFERENCES "Δευτερεύον_προφίλ"("username_de") ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE  "Προφίλ_χρήστη" (
 	"username"	VARCHAR(30) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE  "Προφίλ_χρήστη" (
 CREATE TABLE  "Πρωτεύον_προφίλ" (
 	"username_pro"	varchar(30) NOT NULL,
 	PRIMARY KEY("username_pro"),
-	FOREIGN KEY("username_pro") REFERENCES "Προφίλ χρήστη"("username") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("username_pro") REFERENCES "Προφίλ_χρήστη"("username") ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE  "Συσκευή" (
 	"device_id"	varchar(30) NOT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE "Πραγματοποιεί" (
 	"IP_Address"	varchar(15),
 	PRIMARY KEY("command_id_πραγματοποιεί"),
 	FOREIGN KEY("command_id_πραγματοποιεί") REFERENCES "Εντολή"("command_id") ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY("username_πραγματοποιεί") REFERENCES "Προφίλ χρήστη"("username") ON DELETE SET NULL ON UPDATE CASCADE
+	FOREIGN KEY("username_πραγματοποιεί") REFERENCES "Προφίλ_χρήστη"("username") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 
